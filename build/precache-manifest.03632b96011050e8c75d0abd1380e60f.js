ffa4de14126b4975902d0e0b1ec5cc37029867dc
Davy de Haas <davy.dehaas98@gmail.com>
2020-04-10 17:58:21 +0200

    Feature/dwet 53 (#1)
    * Add .gitignore
    
    * Setup React with Typescript
    
    * Add default GitHub Actions workflow
    
    * Update Typescript version
    
    * Install Redux

---
+++ build/precache-manifest.03632b96011050e8c75d0abd1380e60f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a346df2328cd388953cfdbc8083239c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3d5826e03dfa78d8eeb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b10adf80e567769b699",
+    "url": "/static/js/2.a2008b0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2008b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d5826e03dfa78d8eeb",
+    "url": "/static/js/main.d5922d15.chunk.js"
+  },
+  {
+    "revision": "9be3999de6765e05953c",
+    "url": "/static/js/runtime-main.057b62f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
