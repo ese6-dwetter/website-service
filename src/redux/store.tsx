356453e3b817842ad1156292488708e6f5be82b9
Davy de Haas <davy.dehaas98@gmail.com>
2020-04-27 13:19:05 +0200

    Feature/dwet 64 (#4)

---
+++ src/redux/store.tsx
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { createLogger } from "redux-logger";
+import userReducer from "./user.reducer";
+
+const loggerMiddleware = createLogger();
+
+export default createStore(combineReducers({
+        userReducer,
+    }),
+    {}, // Preloaded state
+    applyMiddleware(
+        thunkMiddleware, // Makes it possible to dispatch
+        loggerMiddleware, // Logs actions
+    )
+);
