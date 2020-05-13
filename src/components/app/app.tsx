46886d0242a6a00c7b7f932bfb90c896f12340a0
Davy de Haas <davy.dehaas98@gmail.com>
2020-05-13 16:30:49 +0200

    DWET-65: Add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from '../../logo.svg';
-import './app.css';
+import React from "react";
+import logo from "../../logo.svg";
+import "./app.css";
+import { Provider } from "react-redux";
+import { Router, Route, Switch } from "react-router-dom";
+import NavigationBar from "../navigation-bar/navigation-bar";
+import store from "../../redux/store";
+import Register from "../register/register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app() {
+    return (
+        <React.StrictMode>
+            <Provider store={store}>
+                <Router>
+                    <div className="app-container">
+                        <NavigationBar />
+                        <main role="main" className={"container"}>
+                            <Switch>
+                                <Route exact path="/" />
+                                <Route path="/register" component={Register} />
+                            </Switch>
+                        </main>
+                    </div>
+                </Router>
+            </Provider>
+        </React.StrictMode>
+    );
 }
 
-export default App;
+export default app;
