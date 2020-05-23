f2ce677b6a2a3e3fe16f3cd1812867440c7121ed
Davy de Haas <davy.dehaas98@gmail.com>
2020-05-23 12:48:56 +0200

    DWET-65: Remove components

--- src/components/app/App.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledContainer = styled.div`
-    background-color: #AAAAAA;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-`;
