6a55151eb38cc44c7603bb1056aec2c44b3773c5
Davy de Haas <davy.dehaas98@gmail.com>
2020-06-29 14:53:07 +0200

    DWET-87: Add Post networking

---
+++ src/networking/post.networking.ts
@@ -0,0 +1,54 @@
+import config from "../config.json";
+import Post from "../entities/Post.entity";
+
+export const postFetch = async (id: string): Promise<Response> => {
+    const options: RequestInit = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        cache: 'default'
+    }
+
+    return await fetch(config.API.POST_SERVICE + '/posts/' + id, options);
+}
+
+export const createPostFetch = async (post: Post, token: string): Promise<Response> => {
+    const options: RequestInit = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        cache: 'default',
+        body: JSON.stringify(post)
+    }
+
+    return await fetch(config.API.POST_SERVICE + '/posts', options);
+}
+
+export const likePostFetch = async (post: Post, token: string): Promise<Response> => {
+    const options: RequestInit = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        cache: 'default'
+    }
+
+    return await fetch(config.API.POST_SERVICE + '/posts/' + post.id + '/likes', options);
+}
+
+export const unlikePostFetch = async (post: Post, token: string): Promise<Response> => {
+    const options: RequestInit = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        cache: 'default'
+    }
+
+    return await fetch(config.API.POST_SERVICE + '/posts/' + post.id + '/likes', options);
+}
