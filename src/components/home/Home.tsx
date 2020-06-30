73a66e759fe383b6b9efc6955066202d4d9205e6
Davy de Haas <davy.dehaas98@gmail.com>
2020-06-30 10:08:47 +0200

    DWET-87: Add timeline

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { connect } from "react-redux";
 import { StyledContainer } from "./Home.styles";
+import { withRouter } from "react-router-dom";
+import Timeline from "../timeline/Timeline";
 
 const Home = (props: any): JSX.Element => {
-    const content = (
+    const content = props.authenticationReducer.isAuthenticated ? (
+        <StyledContainer>
+            <Timeline />
+        </StyledContainer>
+    ) : (
         <StyledContainer>
             <h1>Welcome to Dwetter!</h1>
             <h2>A Twitter clone by Davy de Haas</h2>
@@ -16,4 +23,10 @@ const Home = (props: any): JSX.Element => {
     );
 }
 
-export default Home
+const mapStateToProps = (state: any): any => {
+    return {
+        authenticationReducer: state.authenticationReducer
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Home));
