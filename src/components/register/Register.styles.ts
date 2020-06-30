85c93abc9fe3145c3cc8c2717782fa98c423b3c2
Davy de Haas <davy.dehaas98@gmail.com>
2020-06-30 10:28:16 +0200

    DWET-87: Fix Timeline

--- src/components/register/Register.styles.ts
+++ src/components/register/Register.styles.ts
@@ -13,6 +13,17 @@ export const StyledForm = styled.form`
     background-color: #FFFFFF
 `;
 
+export const StyledContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 100px auto;
+width: 500px;
+padding: 20px;
+border-radius: 20px;
+background-color: #FFFFFF
+`;
+
 export const StyledFormControl = styled(FormControl)`
     margin: 10px;
 `;
